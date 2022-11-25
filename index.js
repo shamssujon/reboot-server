f781608de4e847ee4fc3a1ff674299705680ca9a
Shams Sujon <sujon2762@gmail.com>
2022-11-26 04:53:53 +0600

    Change project name

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send(`Sell Phone Server is running at port ${port}`);
+    res.send(`Reboot Server is running at port ${port}`);
 });
 
 app.listen(port, () => {
-    console.log(`Sell Phone Server is running on port: ${port}`);
+    console.log(`Reboot Server is running on port: ${port}`);
 });
