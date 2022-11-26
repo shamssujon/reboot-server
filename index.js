9ce9e3e1a5139a3065191cdad24280d2ec345e38
Shams Sujon <sujon2762@gmail.com>
2022-11-27 05:03:18 +0600

    Get user from client, send to DB

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 9000;
 
@@ -8,9 +11,37 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send(`Reboot Server is running at port ${port}`);
+	res.send(`Reboot Server is running at port ${port}`);
 });
 
 app.listen(port, () => {
-    console.log(`Reboot Server is running on port: ${port}`);
+	console.log(`Reboot Server is running on port: ${port}`);
 });
+
+// Mongo DB
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.9q7qmdx.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+	try {
+
+        const database = client.db("rebootDB");
+		const usersCollection = database.collection("users");
+
+		// Get user from client, send to DB
+		app.post("/users", async (req, res) => {
+			const user = req.body;
+			const result = await usersCollection.insertOne(user);
+			res.send(result);
+		});
+
+	} finally {
+	}
+};
+
+run().catch(console.dir);
